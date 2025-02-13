TenX-UDC/frameworks_base

SystemUI: Ambient display show battery & cpu temperature [1/2]
Change-Id: I680cb9057577fa6ae401a5a72b3ae3f7378a9861
Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -1047,7 +1047,7 @@ public class KeyguardIndicationController {
             boolean useMisalignmentColor = false;
             mLockScreenIndicationView.setVisibility(View.GONE);
             mTopIndicationView.setVisibility(VISIBLE);
-            CharSequence newIndication;
+            CharSequence newIndication = "";
             boolean setWakelock = false;
 
             if (!TextUtils.isEmpty(mBiometricMessage)) {
@@ -1070,8 +1070,24 @@ public class KeyguardIndicationController {
                 setWakelock = animate;
                 updateChargingIndication();
             } else {
-                newIndication = NumberFormat.getPercentInstance()
-                        .format(mBatteryLevel / 100f);
+                String batteryTemp = com.android.internal.util.tenx.Utils.batteryTemperature(mContext, false);
+                String cpuTemp = com.android.internal.util.tenx.Utils.batteryTemperature(mContext, false);
+                switch (getAmbientShowSettings()) {
+                    case 0: // Show battery level
+                        newIndication = NumberFormat.getPercentInstance()
+                                .format(mBatteryLevel / 100f);
+                        break;
+                    case 1: // Battery level & battery temperature
+                        newIndication = NumberFormat.getPercentInstance()
+                                .format(mBatteryLevel / 100f) +
+                                " | " + batteryTemp;
+                        break;
+                    case 2: // Battery level, battery temperature & cpu temperature
+                        newIndication = NumberFormat.getPercentInstance()
+                                .format(mBatteryLevel / 100f) +
+                                " | " + batteryTemp + " | " + cpuTemp;
+                        break;
+                }
                 setWakelock = false;
             }
 
@@ -1107,6 +1123,11 @@ public class KeyguardIndicationController {
         mLockScreenIndicationView.setVisibility(View.VISIBLE);
         updateLockScreenIndications(animate, getCurrentUser());
         updateChargingIndication();
+    }
+
+    private int getAmbientShowSettings() {
+        return Settings.System.getIntForUser(mContext.getContentResolver(),
+            Settings.System.AMBIENT_SHOW_SETTINGS, 0, UserHandle.USER_CURRENT);
     }
 
     public void updateChargingIndication(boolean visible) {

alioth-aosp/android_frameworks_base

packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -1134,7 +1134,7 @@ public class KeyguardIndicationController {
             boolean useMisalignmentColor = false;
             mLockScreenIndicationView.setVisibility(View.GONE);
             mTopIndicationView.setVisibility(VISIBLE);
-            CharSequence newIndication;
+            CharSequence newIndication = "";
             boolean setWakelock = false;
 
             if (!TextUtils.isEmpty(mBiometricMessage)) {
@@ -1156,8 +1156,24 @@ public class KeyguardIndicationController {
                 newIndication = computePowerIndication();
                 setWakelock = animate;
             } else {
-                newIndication = NumberFormat.getPercentInstance()
-                        .format(mBatteryLevel / 100f);
+                String batteryTemp = com.android.internal.util.crdroid.Utils.batteryTemperature(mContext, false);
+                String cpuTemp = com.android.internal.util.crdroid.Utils.batteryTemperature(mContext, false);
+                switch (getAmbientShowSettings()) {
+                    case 0: // Show battery level
+                        newIndication = NumberFormat.getPercentInstance()
+                                .format(mBatteryLevel / 100f);
+                        break;
+                    case 1: // Battery level & battery temperature
+                        newIndication = NumberFormat.getPercentInstance()
+                                .format(mBatteryLevel / 100f) +
+                                " | " + batteryTemp;
+                        break;
+                    case 2: // Battery level, battery temperature & cpu temperature
+                        newIndication = NumberFormat.getPercentInstance()
+                                .format(mBatteryLevel / 100f) +
+                                " | " + batteryTemp + " | " + cpuTemp;
+                        break;
+                }
                 setWakelock = false;
             }
 
@@ -1192,6 +1208,11 @@ public class KeyguardIndicationController {
         mTopIndicationView.setText(null);
         mLockScreenIndicationView.setVisibility(View.VISIBLE);
         updateLockScreenIndications(animate, getCurrentUser());
+    }
+
+    private int getAmbientShowSettings() {
+        return Settings.System.getIntForUser(mContext.getContentResolver(),
+            Settings.System.AMBIENT_SHOW_SETTINGS, 0, UserHandle.USER_CURRENT);
     }
 
     /**
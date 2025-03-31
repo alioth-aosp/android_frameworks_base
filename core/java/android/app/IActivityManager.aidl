9a3a0a6dddc6254f566a0c35858a2a8370437879
Kshitij Koyande <koyande72@gmail.com>
2025-03-31 09:58:31 +0530

    Revert "Add three fingers swipe actions [2/3]"
    This reverts commit ec09a4fec7602ce67e979f71ddf7e9d57f828a72.

--- core/java/android/app/IActivityManager.aidl
+++ core/java/android/app/IActivityManager.aidl
@@ -1040,12 +1040,6 @@ interface IActivityManager {
     IBinder refreshIntentCreatorToken(in Intent intent);
 
     /**
-     *  Should disable touch if three fingers swipe enabled
-     */
-    boolean isThreeFingersSwipeActive();
-    void setThreeFingersSwipeActive(boolean active);
-
-    /**
      *  Force full screen for devices with cutout
      */
     boolean shouldForceCutoutFullscreen(in String packageName);
--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -1614,9 +1614,6 @@ public class ActivityManagerService extends IActivityManager.Stub
 
     static final HostingRecord sNullHostingRecord =
             new HostingRecord(HostingRecord.HOSTING_TYPE_EMPTY);
-
-    private boolean mThreeFingersSwipeEnabled;
-
     /**
      * Used to notify activity lifecycle events.
      */
@@ -19441,22 +19438,6 @@ public class ActivityManagerService extends IActivityManager.Stub
 
     private IBackupManager getBackupManager() {
         return IBackupManager.Stub.asInterface(ServiceManager.getService(Context.BACKUP_SERVICE));
-    }
-
-    @Override
-    public boolean isThreeFingersSwipeActive() {
-        if (!mThreeFingersSwipeEnabled)
-            return false;
-        synchronized (this) {
-            return Settings.System.getIntForUser(mContext.getContentResolver(),
-                    Settings.System.THREE_FINGER_GESTURE_ACTIVE, 0,
-                    UserHandle.USER_CURRENT) == 1;
-        }
-    }
-
-    @Override
-    public void setThreeFingersSwipeActive(boolean active) {
-        mThreeFingersSwipeEnabled = active;
     }
 
     @Override
